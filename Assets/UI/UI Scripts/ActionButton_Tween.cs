7b2eb054407c89201087483a975e60c6a4089a5d
Hai Lam Dang <hai.dang@ue-germany.de>
2024-09-07 00:57:16 +0200

    [ADD] Working animation for Attack button when selected/deselected

--- Assets/UI/UI Scripts/ActionButton_Tween.cs
+++ Assets/UI/UI Scripts/ActionButton_Tween.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using DG.Tweening;
 using UnityEngine;
 
@@ -15,11 +16,14 @@ public class ActionButton_Tween: MonoBehaviour
     [SerializeField]
     private bool isSelected;
 
+    private Tweener idleTween;
+
 
     // Start is called before the first frame update
     void Start()
     {
         ActionButton_IdleAnimation();
+        isSelected = false;
     }
 
     // Update is called once per frame
@@ -29,11 +33,55 @@ public class ActionButton_Tween: MonoBehaviour
     }
 
     //public so we can access via Button
-    public void ActionButton_IdleAnimation()
+    public void ButtonSelection()
     {
-        var endValue = actionButton.anchoredPosition.y + tweenYValue;
-        actionButton.DOAnchorPosY(endValue, actionButton_SO.TweenDuration, actionButton_SO.TweenSnapping)
+        if (isSelected)
+        {
+            isSelected = false;
+            ActionButton_DeSelectedAnimation();
+        }
+
+        else
+        {
+            isSelected = true;
+            ActionButton_SelectedAnimation();
+        }
+    }
+    private void ActionButton_IdleAnimation()
+    {
+        Sequence idleSequence = DOTween.Sequence();
+
+        var endValueIdle = actionButton.anchoredPosition.y + tweenYValue;
+        idleTween = actionButton.DOAnchorPosY(endValueIdle, actionButton_SO.TweenDuration, actionButton_SO.TweenSnapping)
             .SetEase(actionButton_SO.EaseType)
             .SetLoops(actionButton_SO.LoopAmount, actionButton_SO.LoopType);
     }
+
+    private void ActionButton_SelectedAnimation()
+    {
+        //First stop IdleTween
+        idleTween.Pause();
+
+        //Play pop-up animation
+        var endValueSelected = actionButton.anchoredPosition.y + 100;
+        actionButton.DOAnchorPosY(endValueSelected, actionButton_SO.TweenDuration * 0.5f, actionButton_SO.TweenSnapping);
+
+    }
+
+    private void ActionButton_DeSelectedAnimation()
+    {
+        //Play pop-down animation
+        var endValueSelected = actionButton.anchoredPosition.y - 100;
+        actionButton.DOAnchorPosY(endValueSelected, actionButton_SO.TweenDuration * 0.5f, actionButton_SO.TweenSnapping);
+
+        //Have a bit delay
+        StartCoroutine(DelayIdleAnimation());
+    }
+
+    IEnumerator DelayIdleAnimation()
+    {
+        yield return new WaitForSeconds(actionButton_SO.TweenDuration * 0.5f);
+        idleTween.Play();
+    }
 }
+
