62a3bfc0f8c520aa4715ea5b7eb1caf580bccccd
Jan Teschner <90393366+janteschner@users.noreply.github.com>
2024-09-06 21:35:16 +0200

    added basic classes and attacks

---
+++ Assets/Combat/Code/Attack.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Combat
+{
+    [CreateAssetMenu(menuName = "Attack")]
+    public class Attack : ScriptableObject
+    {
+        [SerializeField] private int baseDamage;
+        [SerializeField] private string attackName;
+        [SerializeField] [CanBeNull] private string attackDescription;
+        [SerializeField] private Element element;
+        [SerializeField] [CanBeNull] private Attack upgradesTo;
+    }
+}
---
+++ Assets/Combat/Code/Element.cs
@@ -0,0 +1,10 @@
+﻿public enum Element
+{
+    UNTYPED = 0,
+    FIRE = 1,
+    ICE = 2,
+    VOLT = 3,
+    SLASH = 4,
+    STAB = 5,
+    BASH = 6
+}
