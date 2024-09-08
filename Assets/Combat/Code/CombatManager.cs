6dd90c153d800c93ce40a428d9a6fd52608f2bc5
LukeBu95 <lukas.buttke@outlook.de>
2024-09-08 12:55:16 +0200

    Anim States for Enemies

--- Assets/Combat/Code/CombatManager.cs
+++ Assets/Combat/Code/CombatManager.cs
@@ -270,7 +270,7 @@ public class CombatManager : MonoBehaviour
         isPlayerTurn = true;
 
         // Elemental Attacks are Shoot
-        if (false)
+        if (ElementFunctions.IsMagicalElement(PlayerManager.Instance.FirstAttack.element))
         {
             PlayerManager.Instance.Animator.SetTrigger("Shoot");
         }
@@ -285,11 +285,11 @@ public class CombatManager : MonoBehaviour
         Debug.Log("Combo being used! The combo is " + _player.CurrentCombo);
         isPlayerTurn = false;
 
-        // // Elemental Attacks are Shoot
-        // if (ElementFunctions.IsMagicalElement(_player.FirstAttack.element))
-        // {
-        //     PlayerManager.Instance.Animator.SetTrigger("Shoot");
-        // }
+        //// Elemental Attacks are Shoot
+        //if (ElementFunctions.IsMagicalElement(_player.FirstAttack.element))
+        //{
+        //    PlayerManager.Instance.Animator.SetTrigger("Shoot");
+        //}
         PlayerManager.Instance.Animator.SetTrigger("Move");
         
     }
@@ -300,7 +300,7 @@ public class CombatManager : MonoBehaviour
         isPlayerTurn = false;
 
         // Elemental attacks are Shoot
-        if (false)
+        if (ElementFunctions.IsMagicalElement(PlayerManager.Instance.SecondAttack.element))
         {
             PlayerManager.Instance.Animator.SetTrigger("Shoot");
         }
